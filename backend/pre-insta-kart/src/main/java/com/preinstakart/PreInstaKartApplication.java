16b13b1dea6959b79f2f2f3b99719a0e063ae91f
Sunny Sharma <sunnysharmagts@gmail.com>
2021-05-03 11:36:04 +0530

    Initial commit

---
+++ backend/pre-insta-kart/src/main/java/com/preinstakart/PreInstaKartApplication.java
@@ -0,0 +1,13 @@
+package com.preinstakart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreInstaKartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreInstaKartApplication.class, args);
+	}
+
+}
---
+++ backend/pre-insta-kart/src/test/java/com/preinstakart/PreInstaKartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preinstakart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreInstaKartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
